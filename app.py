f73da1c1f4564111338fce83b962e3c04a7f5fd7
李揚 <leeyang0403@gmail.com>
2022-10-20 14:27:55 +0800

    add runtime

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 
 natural_language_understanding.set_service_url('https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/37a51fcf-736c-472a-85ed-84f0904cce24')
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 
 
